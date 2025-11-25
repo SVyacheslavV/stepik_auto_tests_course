2de87d17ab529bd5cae1ce2498138fa136800294
SVyacheslavV <suslavik@yandex.ru>
2025-11-25 19:03:59 +0300

    add_lesson1.6_step7

---
+++ lesson1.6_step7.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+try:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/huge_form.html")
+    elements = browser.find_elements(By.TAG_NAME, "input")
+    for element in elements:
+        element.send_keys("заполнил")
+
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+    alert = browser.switch_to.alert
+    print(f'Поздравляю, вы выполнили задание! Скопируйте этот код как ответ на тест Степика: {alert.text.split()[-1]}')
+
+finally:
+    # успеваем скопировать код за 10 секунд
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
